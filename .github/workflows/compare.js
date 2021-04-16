94240c38c35019961be3d50ce1657290b72ce5bb
u9g <43508353+u9g@users.noreply.github.com>
2021-04-16 08:56:45 -0400

    Create sorting workflow (#4)
    * Create blank.yml
    
    * Create compare.js
    
    * Update and rename blank.yml to ci.yml
    
    * Update ci.yml
    
    * Update compare.js

---
+++ .github/workflows/compare.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const fs = require('fs')
+function naturalSort (a, b) {
+  const aPriority = /[a-z]/i.test(a) * 3 + /\d+/i.test(a) * 2
+  const bPriority = /[a-z]/i.test(b) * 3 + /\d+/i.test(b) * 2
+
+  if (aPriority === bPriority) return a.toString().localeCompare(b, 'en', { numeric: true })
+  return aPriority < bPriority ? 1 : -1
+}
+
+const tags = require(path.join('..', '..', 'tags.json'))
+const newTags = {}
+Object.entries(tags).sort(naturalSort).forEach(x => { newTags[x[0]] = x[1] })
+
+fs.writeFileSync(path.join('..', '..', 'tags.json'), JSON.stringify(newTags, null, 2))
